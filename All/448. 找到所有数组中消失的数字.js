5ccb1957cc5ad2f27ff5f25814009d733fa19280
GaoJianXin520 <1603452601@qq.com>
2022-01-10 18:05:42 +0800

    448. 找到所有数组中消失的数字

---
+++ All/448. 找到所有数组中消失的数字.js
@@ -0,0 +1,42 @@
+/*
+找到所有数组中消失的数字
+Category	Difficulty	Likes	Dislikes
+algorithms	Easy (65.14%)	877	-
+Tags
+Companies
+给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
+
+ 
+
+示例 1：
+
+输入：nums = [4,3,2,7,8,2,3,1]
+输出：[5,6]
+示例 2：
+
+输入：nums = [1,1]
+输出：[2]
+ 
+
+提示：
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+进阶：你能在不使用额外空间且时间复杂度为 O(n) 的情况下解决这个问题吗? 你可以假定返回的数组不算在额外空间内。
+
+Discussion | Solution
+
+Code Now
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ let findDisappearedNumbers = nums => {
+    nums.forEach((el, i) => {
+        el > 0 && (nums[Math.abs(el) - 1] *= -1);
+    });
+    return nums.filter(el => el > 0);
+};
